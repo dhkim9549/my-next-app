15e16a00bad96bfaab39fd118fa07ca7ff4408c5
root <dhkim95@hotmail.com>
2025-08-08 01:54:34 +0000

    fix some

--- app/prod-panel.tsx
+++ app/prod-panel.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-async function getRcmdProdData() {
-  return [];
-}
+import { getStockInfo } from './get-stock-info';
+
+import Paper from '@mui/material/Paper';
 
 export default function ProdPanel({ queryObj }) {
   let [rcmdItems, setRcmdItems] = useState([]);
@@ -10,8 +10,9 @@ export default function ProdPanel({ queryObj }) {
   useEffect(() => {
     console.log({ queryObj });
     setRcmdItems([]);
-    getRcmdProdData(queryObj).then((x) => {
-      console.log("x = " + JSON.stringify(x));
+    getStockInfo(queryObj).then((x) => {
+      console.log({x});
+      setRcmdItems(x);
     });
   }, [queryObj]);
 
@@ -22,15 +23,15 @@ export default function ProdPanel({ queryObj }) {
       )}
       <div className="m-4 flex flex-wrap gap-3">
         {rcmdItems.map((x) => (
-          <Paper className="w-full lg:w-96 bg-white p-4 flex flex-col gap-2">
+          <Paper className="w-full lg:w-96 bg-white p-4 flex flex-col gap-2" key={x.종목명}>
             <div className="text-slate-800 text-2xl mt-5 mb-2">
-              {prodInfo.rcmdProdNm}
+              {x.종목명}
             </div>
             <div className="text-slate-900">
-              추천순위: {prodObj.rcmdProrRnk}
+              현재가: {x.현재가}
             </div>
             <div className="text-slate-900">
-              보증구분코드: {prodObj.grntDvcd}
+              등락률: {x.등락률}
             </div>
           </Paper>
         ))}
