83b1a46df2b3300ff8a1e97a87a7b975881e4126
root <dhkim95@hotmail.com>
2025-08-09 06:12:16 +0000

    fix some

--- app/prod-panel.tsx
+++ app/prod-panel.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-import { getStockInfo } from './get-stock-info';
+import { getStockInfo } from "./get-stock-info";
 
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 
 export default function ProdPanel({ queryObj }) {
   let [rcmdItems, setRcmdItems] = useState([]);
@@ -11,7 +11,7 @@ export default function ProdPanel({ queryObj }) {
     console.log({ queryObj });
     setRcmdItems([]);
     getStockInfo(queryObj).then((x) => {
-      console.log({x});
+      console.log({ x });
       setRcmdItems(x);
     });
   }, [queryObj]);
@@ -23,16 +23,13 @@ export default function ProdPanel({ queryObj }) {
       )}
       <div className="m-4 flex flex-wrap gap-3">
         {rcmdItems.map((x) => (
-          <Paper className="w-full lg:w-96 bg-white p-4 flex flex-col gap-2" key={x.종목명}>
-            <div className="text-slate-800 text-2xl mt-5 mb-2">
-              {x.종목명}
-            </div>
-            <div className="text-slate-900">
-              현재가: {x.현재가}
-            </div>
-            <div className="text-slate-900">
-              등락률: {x.등락률}
-            </div>
+          <Paper
+            className="w-full lg:w-96 bg-white p-4 flex flex-col gap-2"
+            key={x.종목명}
+          >
+            <div className="text-slate-800 text-2xl mt-5 mb-2">{x.종목명}</div>
+            <div className="text-slate-900">현재가: {x.현재가}</div>
+            <div className="text-slate-900">등락률: {x.등락률}</div>
           </Paper>
         ))}
       </div>
