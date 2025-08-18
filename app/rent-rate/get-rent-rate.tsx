9d4027b7428fa93b3a4ad2607d300a2443f2f731
root <dhkim95@hotmail.com>
2025-08-18 05:56:23 +0000

    remove tabletojson, add fetch

--- app/rent-rate/get-rent-rate.tsx
+++ app/rent-rate/get-rent-rate.tsx
@@ -14,27 +14,24 @@ export async function getRentRate(query) {
   return resData;
 }
 
-let apiUrl =
-  "https://hf.go.kr/ko/sub02/sub02_01_07_01.do";
+let apiUrl = "http://24.144.81.34:3001/api/get-loan-rate";
 
 async function main(query) {
 
   console.info("main() start...");
   console.info({ query });
 
-  let table = "";
-  await tabletojson.convertUrl(apiUrl, function (tablesAsJson) {
-    table = tablesAsJson[0];
-  });
+  let res = await fetch(apiUrl
+    + "?bnkNm=" + query,
+    { next: { revalidate: 600 } }
+  );
+
+  let table = await res.json();
+  console.info(Object.prototype.toString.call(table));
 
   console.info({ table });
 
-  let table2 = table.filter(function (e) {
-    return e?.금융기관?.startsWith(query);
-  });
-
-  console.info({ table2 });
   console.info("main() end...");
 
-  return table2;
+  return table;
 }
