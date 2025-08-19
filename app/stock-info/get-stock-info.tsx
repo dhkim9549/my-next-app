7e5692facbece504bb5ecd1001cc737df3a455d4
root <dhkim95@hotmail.com>
2025-08-19 02:04:20 +0000

    change from tabletojson to fetch

--- app/stock-info/get-stock-info.tsx
+++ app/stock-info/get-stock-info.tsx
@@ -5,8 +5,6 @@
 
 "use server"
 
-import { tabletojson } from "tabletojson";
-
 export async function getStockInfo(query) {
 
   console.info("getStockInfo() start...");
@@ -19,27 +17,23 @@ export async function getStockInfo(query) {
   return resData;
 }
 
-let apiUrl =
-  "https://api.scraperapi.com/?api_key=a6feef9544ee61c2d5b078836ca638f4&url=https%3A%2F%2Ffinance.naver.com%2Fsise%2Fsise_market_sum.naver";
+let apiUrl = "http://24.144.81.34:3001/api/get-stock-info";
 
 async function main(query) {
 
   console.info("main() start...");
   console.info({ query });
 
-  let table = "";
-  await tabletojson.convertUrl(apiUrl, function (tablesAsJson) {
-    table = tablesAsJson[1];
+  let res = await fetch(apiUrl + "?stockNm=" + query, {
+    next: { revalidate: 60 },
   });
+
+  let table = await res.json();
+  console.info(Object.prototype.toString.call(table));
 
   console.info({ table });
 
-  let table2 = table.filter(function (e) {
-    return e?.종목명?.startsWith(query);
-  });
-
-  console.info({ table2 });
   console.info("main() end...");
 
-  return table2;
+  return table;
 }
